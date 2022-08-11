3284127176342584f15df5907870ea46e5af053b
PunRabbit <tjdmsch2201@gmail.com>
2022-08-11 21:35:19 +0900

    Clova 연동 객체에 대한 사소한 고민 Commit...

--- Server/app/connection/clova/ClovaAbstract.py
+++ Server/app/connection/clova/ClovaAbstract.py
@@ -1,2 +1,9 @@
 from abc import ABCMeta, abstractmethod
+from Server.app.db.clova.ClovaDTO import ClovaDTO
+
+
+class ClovaTextOcrModel(metaclass=ABCMeta):
+    @abstractmethod
+    def request(self) -> ClovaDTO:
+        pass
 
