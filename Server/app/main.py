7266852936f7e89a929dcbc87dfb4ce27cf8a1e0
PunRabbit <tjdmsch2201@gmail.com>
2022-08-12 18:45:14 +0900

    작업 이관용 중간 커밋 (Origin 수정, 로컬 동작 확인)

--- Server/app/main.py
+++ Server/app/main.py
@@ -1,15 +1,15 @@
 import sys
-sys.path.append("./../../../Juniverse")
+sys.path.append("/Users/jun/Juniverse")
 import uvicorn
 from starlette.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from Server.app.core.base.BaseConfig import base_config
+from Server.app.core.base.BaseConfig import BASE_CONFIG
 
 
 app: FastAPI = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[BASE_CONFIG.SERVER_ALLOW_ORIGIN],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -17,10 +17,10 @@ app.add_middleware(
 
 if __name__ == "__main__":
     uvicorn.run(
-        base_config.DEFAULT_BRIDGE,
-        host=base_config.SERVER_URL,
-        port=base_config.SERVER_PORT,
-        workers=base_config.SERVER_WORKER_NUM,
+        BASE_CONFIG.DEFAULT_BRIDGE,
+        host=BASE_CONFIG.SERVER_URL,
+        port=BASE_CONFIG.SERVER_PORT,
+        workers=BASE_CONFIG.SERVER_WORKER_NUM,
         reload=True
     )
 
