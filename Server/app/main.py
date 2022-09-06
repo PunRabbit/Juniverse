d9960039bf44d737d971e7b4d273e7e63ded04e3
PunRabbit <tjdmsch2201@gmail.com>
2022-09-06 19:28:28 +0900

    누락 변경점 Commit

--- Server/app/main.py
+++ Server/app/main.py
@@ -3,13 +3,14 @@ sys.path.append("/Users/jun/Juniverse")
 import uvicorn
 from starlette.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from Server.app.core.configs.BaseConfig import BASE_CONFIG
+from Server.app.core.compact.CompactConfig import CONFIG
+# from Server.app.core.configs.BaseConfig import BASE_CONFIG
 
 
 app: FastAPI = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[BASE_CONFIG.SERVER_ALLOW_ORIGIN],
+    allow_origins=[CONFIG.BASE.SERVER_ALLOW_ORIGIN],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -17,10 +18,10 @@ app.add_middleware(
 
 if __name__ == "__main__":
     uvicorn.run(
-        BASE_CONFIG.DEFAULT_BRIDGE,
-        host=BASE_CONFIG.SERVER_URL,
-        port=BASE_CONFIG.SERVER_PORT,
-        workers=BASE_CONFIG.SERVER_WORKER_NUM,
+        CONFIG.BASE.DEFAULT_BRIDGE,
+        host=CONFIG.BASE.SERVER_URL,
+        port=CONFIG.BASE.SERVER_PORT,
+        workers=CONFIG.BASE.SERVER_WORKER_NUM,
         reload=True
     )
 
