1d579e5c1a1d68d5f648952462266d9c0435deae
PunRabbit <tjdmsch2201@gmail.com>
2022-10-16 15:07:06 +0900

    실행 함수 파일 분리

--- Server/app/core/configs/TestConfig.py
+++ Server/app/core/configs/TestConfig.py
@@ -6,5 +6,6 @@ from Server.app.core.ConfigAbstract import ConfigClass
 @dataclass(frozen=True)
 class TestConfig(ConfigClass):
     SCORE: int = int(os.getenv("TEST_SCORE"))
+    LOAD_DELAY: int = int(os.getenv("TEST_DELAY"))
 
 
--- Server/app/main.py
+++ Server/app/main.py
@@ -1,13 +1,12 @@
 import sys
 import os
-sys.path.append(os.getcwd()[:-11])
-
-import uvicorn
-from unittest import TestResult
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+
+sys.path.append(os.getcwd()[:-11])
+
 from Server.app.core.compact.CompactConfig import CONFIG
-from Server.app.test.compactRunner.CompactRunner import CompactRunnerModule
+from Server.app.runner.MainRunner import main_run
 
 
 app: FastAPI = FastAPI()
@@ -21,28 +20,4 @@ app.add_middleware(
 
 
 if __name__ == "__main__":
-    try:
-        run_flag: str = sys.argv[1]
-        if run_flag == "test":
-            test_result: TestResult = CompactRunnerModule().start_test()
-            stream = os.popen("coverage report")
-            output = stream.read()
-            coverage_int: int = int(output[-4:-2])
-            if coverage_int < CONFIG.TEST.SCORE:
-                print(f"현재 커버리지: {coverage_int}%. 기준 커버리지 {CONFIG.TEST.SCORE}% 에 도달하지 못했습니다.")
-                sys.exit(1)
-            elif test_result.wasSuccessful() is True:
-                print(f"현재 커버리지: {coverage_int}%. 기준 커버리지 {CONFIG.TEST.SCORE}% 를 상회합니다.")
-                uvicorn.run(
-                    CONFIG.BASE.DEFAULT_BRIDGE,
-                    host=CONFIG.BASE.SERVER_URL,
-                    port=CONFIG.BASE.SERVER_PORT,
-                    workers=CONFIG.BASE.SERVER_WORKER_NUM,
-                    reload=True
-                )
-            else:
-                print(f"현재 커버리지: {coverage_int}%. 기준 커버리지 {CONFIG.TEST.SCORE}% 를 넘겼으나 테스트 실패입니다.")
-                sys.exit(1)
-    except IndexError:
-        os.system("coverage run -m main test discover")
-
+    main_run()
